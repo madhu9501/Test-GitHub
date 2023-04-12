6424212848c07b3cd31e1e5373c37e2062d1e41e
maddy <maddy95sharma@gmail.com>
2023-04-12 13:47:27 +0530

    Added class to TestOne.cs

--- TestOne.cs
+++ TestOne.cs
@@ -1,2 +1,13 @@
 using System;
 using UnityEngine;
+
+namespace TestGitHub
+{
+    public class TestClass
+    {
+        private int _id;
+        private string _name;
+        private string _health;
+
+    }
+}
